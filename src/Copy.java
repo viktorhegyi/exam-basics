161837275e45f96cc950b5fb86c2e0fde2282668
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-18 11:43:21 +0200

    add the created Copy.class and add usage.txt

--- src/Copy.java
+++ src/Copy.java
@@ -1,9 +1,35 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by Viktor on 2017-04-18.
  */
 public class Copy {
   public static void main(String[] args) {
 
+    if ( args.length == 0) {
+      usage();
+    } else if ( args.length == 1 ) {
+      System.out.println("No destination provided");
+    } else if ( args.length == 2) {
+    }
+  }
+
+  static void usage() {
+    List<String> usageFile = null;
+
+    try {
+      Path usagePath = Paths.get("usage.txt");
+      usageFile = Files.readAllLines(usagePath);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+    for (int i = 0; i < usageFile.size(); i++) {
+      System.out.println(usageFile.get(i));
+    }
   }
 
 }
